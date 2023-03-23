500875cf7af0d256318d3dcf498b3088d1851abf
Wojciech Gajda <wggajda@gmail.com>
2023-03-23 21:37:01 +0100

    Extended demo

--- src/demo.py
+++ src/demo.py
@@ -11,21 +11,127 @@ robot.__version__()
 
 robot.connect()
 
+orientation = [0,0,0]
+
 # get robot state
 print("Current poses: ")
 cur_jpos = robot.get_curjpos()
 print(f"Current joints: {cur_jpos}")
 
+print("Setting PR[90] to 1,2,3,...")
+robot.set_pr(90,[1,2,3,4,5,6])
+
+print(f"Getting PR[91]: {robot.get_pr(91)}")
+
+print(f"Getting R[91]: {robot.get_reg(90)}")
+
+
 # move in joint space
+
 robot.move(
-    "joint",
-    vals=np.array(cur_jpos) + 0.5,
+    "pose",
+    vals= [0,0,0] + orientation,
     velocity=100,
     acceleration=100,
     cnt_val=0,
     linear=False,
 )
 
+robot.move(
+    "pose",
+    vals= [200,0,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+    linear=True,
+)
+
+robot.move(
+    "pose",
+    vals= [200,200,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+    linear=True,
+)
+
+robot.move(
+    "pose",
+    vals= [0,200,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+    linear=True,
+)
+
+robot.move(
+    "pose",
+    vals=[0,0,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+    linear=True,
+)
+
+robot.move(
+    "pose",
+    vals=[-100,0,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+    linear=True,
+)
+
 print("After: ")
 cur_jpos = robot.get_curjpos()
 print(f"Current joints: {cur_jpos}")
+cur_pos = robot.get_curjpos()
+print(f"Current cartesian: {cur_pos}")
+
+robot.circ(
+    mid=[-71,71,0] + orientation,
+    end=[0,100,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+)
+
+robot.circ(
+    mid=[71,71,0] + orientation,
+    end=[100,0,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+)
+
+robot.circ(
+    mid=[71,-71,0] + orientation,
+    end=[0,-100,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+)
+
+
+robot.circ(
+    mid=[-71,-71,0] + orientation,
+    end=[-100,0,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+)
+
+robot.move(
+    "pose",
+    vals=[0,0,0] + orientation,
+    velocity=100,
+    acceleration=100,
+    cnt_val=0,
+    linear=True,
+)
+
+
+
+
+
+robot.disconnect()
