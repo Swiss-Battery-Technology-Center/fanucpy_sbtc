370659a8b7571ef86274bbfd1831b18b3737edea
rraetz <raphael.raetz@hotmail.com>
2025-04-10 21:31:08 +0200

    grasping demo with digital outputs and cartesian movements

--- src/demo_sbtc.py
+++ src/demo_sbtc.py
@@ -2,7 +2,34 @@ import numpy as np
 from fanucpy import Robot
 import time
 
+import spatialmath as sm
 from threading import Thread, Event
+
+
+def move_cartesian(robot: Robot, pose: sm.SE3, velocity: float = 200, linear: bool=False) -> None:
+    position = list(pose.t * 1000) # Fanuc uses mm
+    orientation = list(pose.rpy(unit="deg"))
+    print(f"Moving to pose: \n{pose}")
+    robot.move(
+        "pose",
+        vals=position + orientation,
+        velocity=velocity,
+        linear=linear,
+    )
+
+def get_pose(robot: Robot) -> sm.SE3:
+    lpos = robot.get_lpos()
+    position = np.array(lpos[:3]) / 1000 # Fanuc uses mm
+    orientation = np.array(lpos[3:])
+    return sm.SE3(position) * sm.SE3.RPY(orientation, unit="deg")
+
+
+def set_vacuum(robot: Robot, enable: bool) -> None:
+    do = 3
+    if enable:
+        robot.set_do(do, False)
+    else:
+        robot.set_do(do, True)
 
 robot = Robot(
     robot_model="Fanuc",
@@ -33,63 +60,35 @@ logger_thread = Thread(target=stream_jpos)
 logger_thread.start()
 
 
-lpos = robot.get_lpos()
-
-for i in range(2):
-    lpos = robot.get_lpos()
-    print(lpos)
-    time.sleep(1)
-
-position = lpos[:3]
-orientation = lpos[3:]
 
 
-# Digital outpus
-for i in range(1, 5):
-    robot.set_do(i, False)
-    print(f"DO {i} set to False")
-    time.sleep(0.5) 
-    robot.set_do(i, True)
-    print(f"DO {i} set to True")
-    time.sleep(0.5)
+pose = get_pose(robot)
+print(f"Current pose: {pose}")
+
+set_vacuum(robot, False)
+
+start_pose = sm.SE3(0.8, 0, 0.4) * sm.SE3.Rx(np.pi)
+move_cartesian(robot, start_pose)
+
+over_battery_pose = sm.SE3(-0.4, 1.0, 0.4) * sm.SE3.Rx(np.pi) * sm.SE3.Rz(-np.pi/2)
+move_cartesian(robot, over_battery_pose)
+
+pre_grasp_pose = sm.SE3(-0.4, 0.98, 0.05) * sm.SE3.Rx(np.pi) * sm.SE3.Rz(-np.pi/2)
+move_cartesian(robot, pre_grasp_pose)
+
+set_vacuum(robot, True)
+
+grasp_pose = pre_grasp_pose * sm.SE3(0, 0, 0.08)
+move_cartesian(robot, grasp_pose, velocity=20, linear=True)
+
+# Move back
+move_cartesian(robot, pre_grasp_pose, velocity=20, linear=True)
+move_cartesian(robot, over_battery_pose, velocity=50)
+move_cartesian(robot, start_pose)
+
+set_vacuum(robot, False)
 
 
-# move in joint space
-print("Move 1")
-new_pos = (np.array(position) + np.array([200, 200, 200])).tolist()
-robot.move(
-    "pose",
-    vals= new_pos + orientation,
-    velocity=200,
-    linear=False,
-    cnt_val=100,
-)
-
-print("Move 3")
-new_pos = (np.array(position) + np.array([0, -300, 200])).tolist()
-robot.move(
-    "pose",
-    vals= new_pos + orientation,
-    velocity=200,
-    linear=True,
-    cnt_val=100,
-)
-
-print("Move 4")
-new_pos = (np.array(position) + np.array([0, -150, 200])).tolist()
-robot.move(
-    "pose",
-    vals= new_pos + orientation,
-    linear=True,
-)
-
-new_pos = (np.array(position)).tolist()
-robot.move(
-    "pose",
-    vals= new_pos + orientation,
-    velocity=200,
-    linear=False,
-)
 
 
 logger_stop_event.set()
@@ -97,94 +96,3 @@ logger_thread.join()
 
 
 
-
-
-# robot.move(
-#     "pose",
-#     vals= [200,0,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-#     linear=True,
-# )
-
-# robot.move(
-#     "pose",
-#     vals= [200,200,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-#     linear=True,
-# )
-
-# robot.move(
-#     "pose",
-#     vals= [0,200,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-#     linear=True,
-# )
-
-# robot.move(
-#     "pose",
-#     vals=[0,0,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-#     linear=True,
-# )
-
-# robot.move(
-#     "pose",
-#     vals=[-100,0,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-#     linear=True,
-# )
-
-# robot.circ(
-#     mid=[-71,71,0] + orientation,
-#     end=[0,100,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-# )
-
-# robot.circ(
-#     mid=[71,71,0] + orientation,
-#     end=[100,0,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-# )
-
-# robot.circ(
-#     mid=[71,-71,0] + orientation,
-#     end=[0,-100,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-# )
-
-
-
-# robot.circ(
-#     mid=[-71,-71,0] + orientation,
-#     end=[-100,0,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-# )
-
-# robot.move(
-#     "pose",
-#     vals=[0,0,0] + orientation,
-#     velocity=v_max,
-#     acceleration=100,
-#     cnt_val=cnt,
-#     linear=True,
-# )
-
-# robot.disconnect()
